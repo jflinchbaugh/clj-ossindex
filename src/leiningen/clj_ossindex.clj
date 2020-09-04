67d914dd7662e48958170eade0676e31b408775f
John Flinchbaugh <jflinchbaugh@sonatype.com>
2020-09-04 00:44:19 -0400

    expand search and filter
    search as maven, clojars, and clojars with doubled name.
    drop any result with no vulnerabilities

--- src/leiningen/clj_ossindex.clj
+++ src/leiningen/clj_ossindex.clj
@@ -2,7 +2,7 @@
   (:require [leiningen.core.classpath :as cp]
             [leiningen.core.eval :as eval]
             [leiningen.deps :as d]
-            [clojure.pprint :as pp]
+            [clojure.pprint :refer [pprint]]
             [clj-http.client :as client]
             [cheshire.core :as json]))
 
@@ -16,26 +16,40 @@
   (map #(take 2 %) (flatten-map (cp/managed-dependency-hierarchy :dependencies :managed-dependencies project)))
   )
 
-(defn- make-purl [[full-name version]]
-  (str "pkg:maven/" (name full-name) "@" version))
+(defn- make-purl [ecosystem [full-name version]]
+  (str "pkg:" ecosystem "/" full-name "@" version))
+
+(defn- double-name [[name version]]
+  [(str name "/" name) version])
+
+(def make-purls 
+  (juxt
+    (partial make-purl "maven")
+    (partial make-purl "clojars")
+    #(make-purl "clojars" (double-name %))))
 
 (defn clj-ossindex
   "I don't do a lot."
   [project & args]
-  (let [classpath (->> project cp/get-classpath (filter #(.endsWith % ".jar")))
-        deps (map make-purl (dependencies project))
+  (let [deps (mapcat
+               make-purls
+               (dependencies project))
         response (client/post
                    "https://ossindex.sonatype.org/api/v3/component-report"
                    {:body (json/generate-string {:coordinates deps})
                     :content-type :json
                     :cookie-policy :none})
         ]
+    (println "Evaluating:")
+    (pprint deps)
+    (println "Vulnerabilities:")
     (if (not= 200 (:status response))
       (do
         (println "FAILED")
-        (pp/pprint response))
-      (pp/pprint (json/parse-string (:body response) true)))
-    ))
+        (pprint response))
+      (pprint (remove #(empty? (:vulnerabilities %)) (json/parse-string (:body response) true))))
+    )
+  )
 
 
 (comment
